16fd18192a6657d6cbe1f7cb856a25d99570da85
ToshiyaKonno <tk.ichinoseki@gmail.com>
2021-10-31 09:55:35 +0900

    Initial Commit

---
+++ MemoApp2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoApp2021
+//
+//  Created by 金野利哉 on 2021/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
